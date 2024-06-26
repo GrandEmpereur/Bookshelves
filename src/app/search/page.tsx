122806fa205b8c54ddd71e5432a3a8afe5f12ae7
Youssef <yosuke2017@hotmail.com>
2024-06-26 17:02:24 +0200

    Add Search Page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,20 +5,25 @@ import axios from 'axios';
 const SearchPage = () => {
     const [users, setUsers] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [filteredUsers, setFilteredUsers] = useState([]);
 
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const token = 'oat_Nw.NlpRNVkxWS12OEIyd1VNT0FpQnVuOGNrWlM4RXVDX0k0WV9nOEJoNzIwNjUwOTkwMjI'; // Remplacez par votre vrai token
-                const response = await axios.get('https://bookish.empereur.me/api/search/users?query=musso', {
+                const token = 'oat_Nw.NlpRNVkxWS12OEIyd1VNT0FpQnVuOGNrWlM4RXVDX0k0WV9nOEJoNzIwNjUwOTkwMjI';
+                const response = await axios.get('https://bookish.empereur.me/api/search/users', {
                     headers: {
                         'Authorization': `Bearer ${token}`,
                         'Content-Type': 'application/json'
                     }
                 });
                 setUsers(response.data);
+                setFilteredUsers(response.data);
+                setIsLoading(false);
             } catch (error) {
                 console.error('Erreur lors de la récupération des utilisateurs :', error.response.data);
+                setIsLoading(false);
             }
         };
 
@@ -30,24 +35,23 @@ const SearchPage = () => {
         setSearchTerm(query);
 
         try {
-            const token = 'oat_Nw.NlpRNVkxWS12OEIyd1VNT0FpQnVuOGNrWlM4RXVDX0k0WV9nOEJoNzIwNjUwOTkwMjI'; // Remplacez par votre vrai token
+            const token = 'oat_Nw.NlpRNVkxWS12OEIyd1VNT0FpQnVuOGNrWlM4RXVDX0k0WV9nOEJoNzIwNjUwOTkwMjI';
             const response = await axios.get(`https://bookish.empereur.me/api/search/users?query=${query}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json'
                 }
             });
-            setUsers(response.data);
+            setFilteredUsers(response.data);
         } catch (error) {
             console.error('Erreur lors de la récupération des utilisateurs :', error.response.data);
         }
     };
 
-    // Afficher la liste filtrée ou non filtrée selon searchTerm
-    const filteredUsers = users.filter(user => {
-        const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
-        return fullName.includes(searchTerm.toLowerCase());
-    });
+
+    if (isLoading) {
+        return <p>Chargement en cours...</p>;
+    }
 
     return (
         <div>
@@ -60,19 +64,30 @@ const SearchPage = () => {
             />
             <h3>Famous authors</h3>
             <ul>
-                {filteredUsers.map(user => (
-                    <li key={user.userId}>
-                        <div>
-                            <p>FirstName: {user.firstName}</p>
-                            <p>LastName: {user.lastName}</p>
-                            {user.profilePicture && <img src={user.profilePicture} alt={`${user.firstName} ${user.lastName}`} style={{ width: '100px'}} />}
-                        </div>
-                    </li>
-                ))}
+                {filteredUsers.length > 0 ? (
+                    filteredUsers.map(user => (
+                        <li key={user.userId}>
+                            <div>
+                                <p>FirstName: {user.firstName}</p>
+                                <p>LastName: {user.lastName}</p>
+                                {user.profilePicture && <img src={user.profilePicture} alt={`${user.firstName} ${user.lastName}`} style={{ width: '100px' }} />}
+                            </div>
+                        </li>
+                    ))
+                ) : (
+                    users.map(user => (
+                        <li key={user.userId}>
+                            <div>
+                                <p>FirstName: {user.firstName}</p>
+                                <p>LastName: {user.lastName}</p>
+                                {user.profilePicture && <img src={user.profilePicture} alt={`${user.firstName} ${user.lastName}`} style={{ width: '100px' }} />}
+                            </div>
+                        </li>
+                    ))
+                )}
             </ul>
             <h3>Trending Books</h3>
         </div>
-
     );
 };
 
