c5bf60dd2413e164a4ea5af6f57d850709c5037f
Cedric <cedric.jamme@edu.devinci.fr>
2024-06-27 09:13:52 +0200

    add base style search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,19 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import axios from 'axios';
 
-const SearchPage = () => {
-    const [users, setUsers] = useState([]);
+interface User {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    profilePicture: string;
+}
+
+const SearchPage: React.FC = () => {
+    const [users, setUsers] = useState<User[]>([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [isLoading, setIsLoading] = useState(true);
-    const [filteredUsers, setFilteredUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -30,7 +37,7 @@ const SearchPage = () => {
         fetchUsers();
     }, []);
 
-    const handleSearchChange = async (event) => {
+    const handleSearchChange = async (event: ChangeEvent<HTMLInputElement>) => {
         const query = event.target.value;
         setSearchTerm(query);
 
@@ -48,13 +55,12 @@ const SearchPage = () => {
         }
     };
 
-
     if (isLoading) {
         return <p>Chargement en cours...</p>;
     }
 
     return (
-        <div>
+        <div style={{ fontFamily: 'Roboto, sans-serif' }}>
             <h1>Search</h1>
             <input
                 type="text"
@@ -63,24 +69,46 @@ const SearchPage = () => {
                 onChange={handleSearchChange}
             />
             <h3>Famous authors</h3>
-            <ul>
+            <ul style={{ listStyleType: 'none', padding: 0, display: 'flex', flexWrap: 'wrap' }}>
                 {filteredUsers.length > 0 ? (
                     filteredUsers.map(user => (
-                        <li key={user.userId}>
-                            <div>
-                                <p>FirstName: {user.firstName}</p>
-                                <p>LastName: {user.lastName}</p>
-                                {user.profilePicture && <img src={user.profilePicture} alt={`${user.firstName} ${user.lastName}`} style={{ width: '100px' }} />}
+                        <li key={user.userId} style={{ margin: '10px' }}>
+                            <div style={{ textAlign: 'center' }}>
+                                {user.profilePicture && (
+                                    <img
+                                        src={user.profilePicture}
+                                        alt={`${user.firstName} ${user.lastName}`}
+                                        style={{
+                                            width: '100px',
+                                            height: '100px',
+                                            borderRadius: '50%',
+                                            objectFit: 'cover'
+                                        }}
+                                    />
+                                )}
+                                <p>{user.firstName}</p>
+                                <p>{user.lastName}</p>
                             </div>
                         </li>
                     ))
                 ) : (
                     users.map(user => (
-                        <li key={user.userId}>
-                            <div>
-                                <p>FirstName: {user.firstName}</p>
-                                <p>LastName: {user.lastName}</p>
-                                {user.profilePicture && <img src={user.profilePicture} alt={`${user.firstName} ${user.lastName}`} style={{ width: '100px' }} />}
+                        <li key={user.userId} style={{ margin: '10px' }}>
+                            <div style={{ textAlign: 'center' }}>
+                                {user.profilePicture && (
+                                    <img
+                                        src={user.profilePicture}
+                                        alt={`${user.firstName} ${user.lastName}`}
+                                        style={{
+                                            width: '100px',
+                                            height: '100px',
+                                            borderRadius: '50%',
+                                            objectFit: 'cover'
+                                        }}
+                                    />
+                                )}
+                                <p>{user.firstName}</p>
+                                <p>{user.lastName}</p>
                             </div>
                         </li>
                     ))
